83210ae134352aa40233e1918c530031450168c3
Brunilda Metaj <brunildametaj@MBPvonBrunilda.fritz.box>
2022-05-03 16:12:28 +0200

    Relative path

--- server/dist/routes/employees.js
+++ server/dist/routes/employees.js
@@ -3,29 +3,31 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteEmployee = exports.addEmployee = exports.getEmployee = exports.getEmployees = void 0;
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 const router = express.Router();
+const usersFile = path.join(__dirname, '..', '..', `users.json`);
 const getEmployees = router.get('/', (req, res) => {
-    const users = fs.readFileSync('/Users/brunildametaj/Desktop/springfield/server/src/users.json').toString();
+    const users = fs.readFileSync(usersFile).toString();
     res.send(users);
 });
 exports.getEmployees = getEmployees;
 const getEmployee = router.get('/:id', (req, res) => {
-    const users = fs.readFileSync('/Users/brunildametaj/Desktop/springfield/server/src/users.json').toString();
+    const users = fs.readFileSync(usersFile).toString();
     res.send(JSON.parse(users).filter((user) => user.id === req.params.id));
 });
 exports.getEmployee = getEmployee;
 const addEmployee = router.post('/', (req, res) => {
-    const users = JSON.parse(fs.readFileSync('/Users/brunildametaj/Desktop/springfield/server/src/users.json').toString());
+    const users = JSON.parse(fs.readFileSync(usersFile).toString());
     const newUser = Object.assign(Object.assign({}, req.body), { id: Math.floor(Math.random() * 1000) });
     users.push(newUser);
-    fs.writeFileSync('/Users/brunildametaj/Desktop/springfield/server/src/users.json', JSON.stringify(users));
+    fs.writeFileSync(usersFile, JSON.stringify(users));
     res.send(users);
 });
 exports.addEmployee = addEmployee;
 const deleteEmployee = router.delete('/:id', (req, res) => {
-    const users = fs.readFileSync('/Users/brunildametaj/Desktop/springfield/server/src/users.json');
+    const users = fs.readFileSync(usersFile);
     const filteredUsers = JSON.parse(users.toString()).filter((user) => user.id.toString() !== req.params.id.toString());
-    fs.writeFileSync('/Users/brunildametaj/Desktop/springfield/server/src/users.json', JSON.stringify(filteredUsers));
+    fs.writeFileSync(usersFile, JSON.stringify(filteredUsers));
     res.send(filteredUsers);
 });
 exports.deleteEmployee = deleteEmployee;
--- server/src/routes/employees.ts
+++ server/src/routes/employees.ts
@@ -3,28 +3,30 @@ import fs = require("fs");
 import path = require("path");
 const router = express.Router();
 
+const usersFile = path.join(__dirname, '..', '..', `users.json`);
+
 const getEmployees = router.get('/', (req: any, res: any) => {
-  const users = fs.readFileSync('/Users/brunildametaj/Desktop/springfield/server/src/users.json').toString();
+  const users = fs.readFileSync(usersFile).toString();
   res.send(users);
 });
 
 const getEmployee = router.get('/:id', (req: any, res: any) => {
-  const users = fs.readFileSync('/Users/brunildametaj/Desktop/springfield/server/src/users.json').toString();
+  const users = fs.readFileSync(usersFile).toString();
   res.send(JSON.parse(users).filter((user: any) => user.id === req.params.id));
 });
 
 const addEmployee = router.post('/', (req: any, res: any) => {
-  const users = JSON.parse(fs.readFileSync('/Users/brunildametaj/Desktop/springfield/server/src/users.json').toString());
+  const users = JSON.parse(fs.readFileSync(usersFile).toString());
   const newUser = { ...req.body, id: Math.floor(Math.random() * 1000) }
   users.push(newUser);
-  fs.writeFileSync('/Users/brunildametaj/Desktop/springfield/server/src/users.json', JSON.stringify(users));
+  fs.writeFileSync(usersFile, JSON.stringify(users));
   res.send(users);
 });
 
 const deleteEmployee = router.delete('/:id', (req: any, res: any) => {
-  const users = fs.readFileSync('/Users/brunildametaj/Desktop/springfield/server/src/users.json');
+  const users = fs.readFileSync(usersFile);
   const filteredUsers = JSON.parse(users.toString()).filter((user: any) => user.id.toString() !== req.params.id.toString());
-  fs.writeFileSync('/Users/brunildametaj/Desktop/springfield/server/src/users.json', JSON.stringify(filteredUsers));
+  fs.writeFileSync(usersFile, JSON.stringify(filteredUsers));
   res.send(filteredUsers);
 });
 
